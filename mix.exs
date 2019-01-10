c2f6e0e6cef153ee2bc7559b795320415a8020a3
Norberto Oliveira Junior <norbejunior@gmail.com>
2019-01-10 12:40:48 -0200

    Use coherence lib from branch upgrade-to-phoenix-1.4

--- mix.exs
+++ mix.exs
@@ -43,7 +43,7 @@ defmodule CoherenceDemo.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:coherence, path: "../coherence"},
+      {:coherence, github: "appprova/coherence", branch: "upgrade-to-phoenix-1.4"},
     ]
   end
 
